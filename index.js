453ca8ce98c802e4938f3f6a8ddea1fe431337c9
Dan Mercer <danielmercer1@gmail.com>
2017-09-20 15:13:18 -0600

    Added code to dismiss modal when click outside

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 window.onload = function() {
 	var btn = document.querySelector("button.sign-up");
-	btn.onclick = function() {
-		document.querySelector(".modal-container")
-				.classList.add("active");
+	var scrim = document.querySelector(".modal-container");
+
+	// Show modal when button is clicked.
+	btn.onclick = showModal;
+
+	scrim.onclick = function(e) {
+		// Only dismiss on click on scrim (not click on the modal itself)
+		if (e.target === scrim) {
+			hideModal();
+		}
+	}
+
+	function showModal() {
+		scrim.classList.add("active");
+	}
+
+	function hideModal() {
+		scrim.classList.remove("active");
 	}
 }
